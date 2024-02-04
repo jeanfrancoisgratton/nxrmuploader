c52d66a0f4c3ab576fac7b879f523dad6a5b79bb
RPM Builder <builder@famillegratton.net>
2024-02-04 14:50:02 -0500

    Another build script fix

--- rpmbuild-deps.sh
+++ rpmbuild-deps.sh
@@ -5,7 +5,7 @@ grep ^BuildRequires "nxrmuploader.spec" |awk -F\: '{print "sudo dnf install -y"$
 echo;echo;echo "Done. Now installing the Go binaries"
 
 echo "Fetching archive..."
-sudo wget -q https://go.dev/dl/go1.21.6.linux-amd64.tar.gz -O /opt/go.tar.gz
+sudo wget -q https://go.dev/dl/go1.21.6.linux-amd64.tar.gz -O /tmp/go.tar.gz
 
 echo "Unarchiving..."
 cd /opt ; sudo rm -rf go;sudo tar zxf /tmp/go.tar.gz; sudo rm -f /tmp/go.tar.gz
